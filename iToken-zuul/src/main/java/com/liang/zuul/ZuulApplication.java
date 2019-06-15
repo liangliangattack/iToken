630101241d46c34a24f6f8b8606292e0e05b9be2
e <1364744931@qq.com>
2019-06-15 18:08:14 +0800

    iToken

---
+++ iToken-zuul/src/main/java/com/liang/zuul/ZuulApplication.java
@@ -0,0 +1,15 @@
+package com.liang.zuul;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
